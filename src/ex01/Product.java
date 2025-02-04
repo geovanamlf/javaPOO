fff7280b23b37c04d26420b5dfa3440ce1e8f8d3
geovanamlf <geovanamlfdev@gmail.com>
2025-02-03 22:32:40 -0300

    solving exercise 01

---
+++ src/ex01/Product.java
@@ -0,0 +1,32 @@
+package ex01;
+
+public class Product {
+
+    //Atributos
+    public String name;
+    public double price;
+    public int quantity;
+
+    public double totalValueInStock() { // Metodo para calcular o valor total em estoque.
+        return price * quantity;
+    }
+
+    public void addProducts(int quantity) { // Metodo para adicionar produtos no estoque.
+        this.quantity += quantity;
+    }
+
+    public void removeProducts(int quantity) { // Metodo para remover produtos no estoque.
+        this.quantity -= quantity;
+    }
+
+    public String toString() {
+        return name
+                + ", $ "
+                + String.format("%.2f", price)
+                + ", "
+                + quantity
+                + " units, Total: $ "
+                + String.format("%.2f", totalValueInStock());
+    }
+
+}
---
+++ src/ex01/programProduct.java
@@ -0,0 +1,41 @@
+package ex01;
+
+import java.util.Scanner;
+import java.util.Locale;
+
+public class programProduct {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        Locale.setDefault(Locale.US);
+
+        Product product = new Product();
+
+        System.out.println("Enter product data: ");
+
+        System.out.println("Name: ");
+        product.name = sc.next();
+
+        System.out.println("Price: ");
+        product.price = sc.nextDouble();
+
+        System.out.println("Quantity in stock: ");
+        product.quantity = sc.nextInt();
+
+        System.out.println("Product data: " + product);
+
+        System.out.println("Enter the number of products to be added in stock: ");
+        int quantity = sc.nextInt();
+        product.addProducts(quantity);
+
+        System.out.println("Product data: " + product);
+
+        System.out.println("Enter the number of products to remove from stock: ");
+        quantity = sc.nextInt();
+        product.removeProducts(quantity);
+
+        System.out.println("Updated data: " + product);
+
+        sc.close();
+    }
+}
