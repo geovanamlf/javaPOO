1b11e0794fd7d00e9725e7decc0e204e39659529
geovanamlf <geovanamlfdev@gmail.com>
2025-02-19 20:21:55 -0300

    construtor

--- src/ex01/Product.java
+++ src/ex01/Product.java
@@ -7,6 +7,12 @@ public class Product {
     public double price;
     public int quantity;
 
+    public Product(String name, double price, int quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
     public double totalValueInStock() { // Metodo para calcular o valor total em estoque.
         return price * quantity;
     }
--- src/ex01/programProduct.java
+++ src/ex01/programProduct.java
@@ -9,23 +9,23 @@ public class programProduct {
         Scanner sc = new Scanner(System.in);
         Locale.setDefault(Locale.US);
 
-        Product product = new Product();
-
         System.out.println("Enter product data: ");
 
         System.out.println("Name: ");
-        product.name = sc.next(); // Salvando o input do usuario diretamento no atibuto da classe product
+        String name = sc.next(); // Salvando o input do usuario diretamento no atibuto da classe product
 
         System.out.println("Price: ");
-        product.price = sc.nextDouble();
+        double price = sc.nextDouble();
 
         System.out.println("Quantity in stock: ");
-        product.quantity = sc.nextInt();
+        int quantity = sc.nextInt();
+
+        Product product = new Product(name, price, quantity);
 
         System.out.println("Product data: " + product);
 
         System.out.println("Enter the number of products to be added in stock: ");
-        int quantity = sc.nextInt();
+        quantity = sc.nextInt();
         product.addProducts(quantity);
 
         System.out.println("Product data: " + product);
